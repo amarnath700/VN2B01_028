448a9a0a16f6294bb466d518c4bf186ded277818
amarnath700 <amarnathragipindi@gmail.com>
2022-01-10 11:17:45 +0530

    dic

--- _07_core_python/_09_Data_Structures/_09_Data_Structures/_04_Tuple/Self notes/tuple.py
+++ _07_core_python/_09_Data_Structures/_09_Data_Structures/_04_Tuple/Self notes/tuple.py
@@ -7,6 +7,58 @@ A tuple is a collection which is ordered and unchangeable.
 
 Tuples are written with round brackets.
 Tuple items are ordered, unchangeable, and allow duplicate values.
+Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
+But there are some workarounds.
 
 
+Change Tuple Values:
+Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+But there is a workaround. You can convert the tuple into a list, change the list,
+and convert the list back into a tuple.
 """
+x = (1, 2.5, 3, 1, "Amar", "10")
+print(x)
+print(type(x))
+print(id(x))
+
+# index
+print(x[1])
+print(x[-1])
+
+# slicing
+print(x[:4])
+print(x[-4:])
+print(x[-4:4])
+
+i = 0
+for i in range(len(x)):
+    print(x[i])
+    i = i + 1
+
+# length
+print(len(x))
+
+# membership
+if "Amar" not in x:
+    print(id('Amar'))
+
+# by using type casting we can Add values into tuple.below are steps
+# convert into list.
+# add value to list.
+# convert into tuple.
+
+print("*****  Tuple value  *****")
+print(x)
+x = list(x)  # converting tuple to list
+print("convert tuple to list:", x, type(x))
+x.append([1000])  # by using append adding value into list
+print("list after append", x, type(x))
+x.extend([25, "nath"])  # by using extend adding value into list
+print("list:", x, type(x))
+x = tuple(x)  # converting list to tuple
+print("tuple:", x, type(x))
+
+
+x = x + x
+print(x)
+
