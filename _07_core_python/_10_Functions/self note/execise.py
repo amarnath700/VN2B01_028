31a1ce3e695a7f0a6f791bd98f6d50980dfb1adf
amarnath700 <amarnathragipindi@gmail.com>
2022-01-18 15:30:29 +0530

    h

--- _07_core_python/_10_Functions/self note/execise.py
+++ _07_core_python/_10_Functions/self note/execise.py
@@ -28,7 +28,7 @@ def show_employee(name,salary=9000):
 
 show_employee("amar")
 show_employee("viveka",100000)
-"""
+
 
 
 # Create an inner function to calculate the addition in the following way
@@ -45,3 +45,116 @@ def outer(a, b):
 
 x = outer(20, 30)
 print(x)
+
+
+
+# Define a function that accepts 2 values and return its sum, subtraction and multiplication.
+def x(a, b):
+    h = a + b
+    i = a - b
+    j = a * b
+    print("sum:",h,"sub:",i, "mul:",j)
+
+
+a = int(input("enter 'a' value:"))
+b = int(input("enter 'b' value"))
+x(a, b)
+
+#  Define a function that accepts roll number and returns whether the student is present or absent.
+attendance = [12345, 23456, 34567, 45678, 56789, 67890, 78901, 89012, 90123]
+
+
+def roll_no(x):
+    if x in attendance:
+        print(x, ":student is present")
+    else:
+        print(x, ":student is absent")
+
+
+x = int(input("enter roll number:"))
+roll_no(x)
+
+# Define a function in python that accepts 3 values and returns the maximum of three numbers.
+def value(a, b, c):
+    if a < b < c:
+        print(c, "maximum value")
+    elif a > b > c:
+        print(a, "maximum value")
+    else:
+        print(b, "maximum value")
+
+
+a = int(input("enter 'a' value:"))
+b = int(input("enter 'b' value:"))
+c = int(input("enter 'c' value:"))
+value(a, b, c)
+
+
+
+# Define a function that accepts a number and returns whether the number is even or odd.
+def number(a):
+    if a % 2 == 0:
+        print("Even number:", a)
+    else:
+        print("odd number:", a)
+
+
+a = int(input("enter 'a' value:"))
+number(a)
+
+
+
+# Define a function which counts vowels and consonant in a word.
+def word(x):
+    v = 0
+    c = 0
+    for i in range(len(x)):
+        if x[i] in ['a', 'e', 'i', 'o', 'u']:
+            v = v + 1
+        else:
+            c = c + 1
+
+    print("count of vowels:", v)
+    print("count of consonant:", c)
+
+
+x = input("enter word--:")
+word(x)
+
+
+
+# Define a function that returns Factorial of a number.
+def factorial(x):
+    y = 1
+    for i in range(1, x):
+        x = x * i
+    print(x)
+
+
+x = int(input("enter factorial value:"))
+factorial(x)
+
+
+
+# Define a function that accepts lowercase words and returns uppercase words.
+
+def upper(x):
+    y = x.upper()
+    print(y)
+
+
+x = input("enter lower case word:")
+upper(x)
+
+
+
+# Define a function that accepts radius and returns the area of a circle.
+# A = pi * r^2
+def area_circle(r):
+    area = 3.14 * (r ** 2)
+    print(area)
+
+
+r = int(input("enter the radius:"))
+area_circle(r)
+"""
